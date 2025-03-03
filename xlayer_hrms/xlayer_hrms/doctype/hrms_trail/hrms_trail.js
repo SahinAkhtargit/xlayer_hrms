d317d6c17b5ae5c172a639fbb4126a06e97e9b95
SahinAkhtargit <sahinakhtarinfotech@gmail.com>
2025-03-03 16:37:00 +0530

    hrms trail doctype added in the app

---
+++ xlayer_hrms/xlayer_hrms/doctype/hrms_trail/hrms_trail.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Xlayer Technologies Pvt Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("HRMS Trail", {
+// 	refresh(frm) {
+
+// 	},
+// });
